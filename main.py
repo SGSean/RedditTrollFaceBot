d119c4831cb58e0873c2a48e1c7b47fdaed33886
SGSean <50642391+SGSean@users.noreply.github.com>
2019-05-29 09:37:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import discord.py
+import time
+import tkinter
+bot = discord.Client()
+win = Tk()
+win.configure(width=900, height=1200, backround=#2D2321)
+class discord.Message
+msg = ("test")              
+              
+def send():
+  
